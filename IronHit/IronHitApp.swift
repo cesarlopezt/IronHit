6bf982a7fd2086e39d1a4d3be890be119ee6b504
cesarlopezt <cesarlopez2808@gmail.com>
2023-04-10 22:28:31 -0500

    Initial Commit

---
+++ IronHit/IronHitApp.swift
@@ -0,0 +1,17 @@
+//
+//  IronHitApp.swift
+//  IronHit
+//
+//  Created by Cesar Lopez on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct IronHitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
